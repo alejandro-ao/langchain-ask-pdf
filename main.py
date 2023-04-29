3508b2dff79bf85ce11f7afb8b096c5d5f3b25af
Alejandro <aavila0707@gmail.com>
2023-04-29 22:57:39 +0200

    step1: import your api key

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+
 def main():
     # get api key
+    load_dotenv()
+    openai_key = os.environ['OPENAI_KEY']
+    print(openai_key)
     # initialize pdf reader
     # get raw text from pdf
     # split text into chunks for token limit
