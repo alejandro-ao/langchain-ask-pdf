5cf58289881bc48a2b262e1817953b3da56af315
Alejandro <aavila0707@gmail.com>
2023-04-29 22:43:33 +0200

    create todo

---
+++ main.py
@@ -0,0 +1,12 @@
+def main():
+    # get api key
+    # initialize pdf reader
+    # get raw text from pdf
+    # split text into chunks for token limit
+    # get embeddings from openai
+    # perform docsearch
+    # ask question to llm using langchain
+
+
+if __name__ == '__main__':
+    main()
