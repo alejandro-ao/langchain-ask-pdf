eb58d993164fa275813aac2cda170238b444fe4f
Alejandro <aavila0707@gmail.com>
2023-05-01 16:29:59 +0200

    finish tutorial

--- app.py
+++ app.py
@@ -1,69 +1,55 @@
-# pip install langchain openai pypdf2 faiss-cpu python-dotenv tiktoken
-
 from dotenv import load_dotenv
+import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
-import streamlit as st
+from langchain.callbacks import get_openai_callback
 
 
-def get_text_chunks_from_pdf(pdf_path):
-    # initialize pdf reader
-    pdf = PdfReader('gpt4-in-medicine.pdf')
-
-    # get raw text from pdf
-    raw_text = ""
-    for page in pdf.pages:
-        raw_text += page.extract_text()
-    
-    # split text into chunks for token limit
-    text_splitter = CharacterTextSplitter(        
-        separator = "\n",
-        chunk_size = 1000,
-        chunk_overlap  = 200,
-        length_function = len,
-    )
-    return text_splitter.split_text(raw_text)
-
-
-def setup_text_search(text_chunks):
-    # get embeddings from openai
-    embeddings = OpenAIEmbeddings()
-    
-    # perform docsearch
-    docsearch = FAISS.from_texts(text_chunks, embeddings)
-    chain = load_qa_chain(OpenAI(), chain_type='stuff')
-    
-    return docsearch, chain
-    
-    
-def answer_question(question, docsearch, chain):
-    # ask question to llm using langchain
-    docs = docsearch.similarity_search(question)
-    response = chain.run(input_documents=docs, question=question)
-    return response
-
-
-def app():
-    # get api key
+def main():
     load_dotenv()
-    st.set_page_config(page_title="Chat with your PDFs", page_icon="🧊", layout="wide")
-    st.header("Chat with your PDF")
+    st.set_page_config(page_title="Ask your PDF")
+    st.header("Ask your PDF 💬")
     
-    pdf = st.file_uploader("Upload a PDF", type="pdf")
+    # upload file
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
     
+    # extract the text
     if pdf is not None:
-      text_chunks = get_text_chunks_from_pdf('gpt4-in-medicine.pdf')
-      docsearch, chain = setup_text_search(text_chunks)
-
-      user_question = st.text_input("Ask a question")
+      pdf_reader = PdfReader(pdf)
+      text = ""
+      for page in pdf_reader.pages:
+        text += page.extract_text()
+        
+      # split into chunks
+      text_splitter = CharacterTextSplitter(
+        separator="\n",
+        chunk_size=1000,
+        chunk_overlap=200,
+        length_function=len
+      )
+      chunks = text_splitter.split_text(text)
+      
+      # create embeddings
+      embeddings = OpenAIEmbeddings()
+      knowledge_base = FAISS.from_texts(chunks, embeddings)
+      
+      # show user input
+      user_question = st.text_input("Ask a question about your PDF:")
       if user_question:
-        response = answer_question(user_question, docsearch, chain)
+        docs = knowledge_base.similarity_search(user_question)
+        
+        llm = OpenAI()
+        chain = load_qa_chain(llm, chain_type="stuff")
+        with get_openai_callback() as cb:
+          response = chain.run(input_documents=docs, question=user_question)
+          print(cb)
+           
         st.write(response)
-
+    
 
 if __name__ == '__main__':
-    app()
+    main()
