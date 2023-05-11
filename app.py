e85e3777e92e07907cce07bbea443cfa1b3bf41d
Kim <iyai@naver.com>
2023-05-11 20:52:35 +0900

    to support azure open ai / fix requirements.txt

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from dotenv import load_dotenv
 import streamlit as st
 from PyPDF2 import PdfReader
@@ -5,51 +7,86 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.chains.question_answering import load_qa_chain
+from langchain.llms import AzureOpenAI
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 
 
-def main():
+def streamlit_config():
     load_dotenv()
+
     st.set_page_config(page_title="Ask your PDF")
     st.header("Ask your PDF 💬")
-    
-    # upload file
-    pdf = st.file_uploader("Upload your PDF", type="pdf")
-    
+
+
+def pdf_to_chunks(pdf):
+    chunks = []
     # extract the text
     if pdf is not None:
-      pdf_reader = PdfReader(pdf)
-      text = ""
-      for page in pdf_reader.pages:
-        text += page.extract_text()
-        
-      # split into chunks
-      text_splitter = CharacterTextSplitter(
-        separator="\n",
-        chunk_size=1000,
-        chunk_overlap=200,
-        length_function=len
-      )
-      chunks = text_splitter.split_text(text)
-      
-      # create embeddings
-      embeddings = OpenAIEmbeddings()
-      knowledge_base = FAISS.from_texts(chunks, embeddings)
-      
-      # show user input
-      user_question = st.text_input("Ask a question about your PDF:")
-      if user_question:
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+
+        # split into chunks
+        text_splitter = CharacterTextSplitter(
+            separator="\n",
+            chunk_size=1000,
+            chunk_overlap=200,
+            length_function=len
+        )
+        chunks = text_splitter.split_text(text)
+
+    return chunks
+
+
+def openai_main(chunks):
+    # create embeddings
+    embeddings = OpenAIEmbeddings()
+    knowledge_base = FAISS.from_texts(chunks, embeddings)
+
+    # show user input
+    user_question = st.text_input("Ask a question about your PDF:")
+    if user_question:
         docs = knowledge_base.similarity_search(user_question)
-        
+
         llm = OpenAI()
         chain = load_qa_chain(llm, chain_type="stuff")
         with get_openai_callback() as cb:
-          response = chain.run(input_documents=docs, question=user_question)
-          print(cb)
-           
+            response = chain.run(input_documents=docs, question=user_question)
+            print(cb)
+
         st.write(response)
-    
+
+
+def azure_openai_main(chunks):
+    # create embeddings
+    embeddings = OpenAIEmbeddings(deployment=os.getenv("EMBEDDING_DEPLOYMENT_NAME"), model="text-embedding-ada-002", chunk_size=1)
+    knowledge_base = FAISS.from_texts(chunks, embeddings)
+
+    # show user input
+    user_question = st.text_input("Ask a question about your PDF:")
+    if user_question:
+        docs = knowledge_base.similarity_search(user_question)
+
+        llm = AzureOpenAI(model_name=os.getenv("CHAT_DEPLOYMENT_NAME"))
+        chain = load_qa_chain(llm, chain_type="stuff")
+        with get_openai_callback() as cb:
+            response = chain.run(input_documents=docs, question=user_question)
+            print(cb)
+
+        st.write(response)
+
 
 if __name__ == '__main__':
-    main()
+    open_ai_type = os.getenv("OPENAI_API_TYPE")
+
+    streamlit_config()
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
+
+    chunks = pdf_to_chunks(pdf)
+    if chunks:
+        if open_ai_type == 'azure':
+            azure_openai_main(chunks)
+        else:
+            openai_main(chunks)
